caea634b589182e5c140080f49845b2335fdc81e
inyeongjang <726jang@gmail.com>
2025-07-14 04:39:46 +0900

    init: 프로젝트 초기 설정 (React + Express)

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const indexRouter = require('./routes');
+
+const app = express();
+app.set('port', process.env.PORT || 3002);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false, 
+    saveUninitialized: false, 
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true, 
+        secure: false,
+    },
+}));
+
+app.use('/', indexRouter);
+
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
