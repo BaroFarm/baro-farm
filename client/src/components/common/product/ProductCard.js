3fd08047f5382f0679437ba40e1db497789c6c34
Encho <sunbi2003@gmail.com>
2025-07-16 03:22:57 +0900

    feat: 상품카드 UI만 추가(API 확인 필요)

--- client/src/components/common/product/ProductCard.js
+++ client/src/components/common/product/ProductCard.js
@@ -1,1 +1,34 @@
 //만들고 -> 상품 추천 리스트 짜고 -> 카테고리별 페이지 이동 링크 설정
+import React from 'react';
+
+export default function ProductCard({ product }) {
+    // 별점 수에 따라 ⭐ 반복
+    const stars = '⭐'.repeat(Math.round(product.rating || 0));
+
+    return (
+        <div className="w-[200px] border rounded p-3"
+        style={{
+            width: 232,
+            height: 250,
+            backgroundColor: "#eee",
+            display: "flex", flexDirection: "column",
+            justifyContent: "start",
+            color: "#555",
+            }}>
+            {/* 이미지 */}
+            <img src={product.image} alt={product.name} 
+                style={{ width: "100%", height: 150, objectFit: "cover" }} />
+            {/* 상품명 */}
+            <h3 style={{ marginTop: 8, fontWeight: "600", fontSize: 15, textAlign: "left" }}>
+                {product.name}</h3>
+            {/* 가격 */}
+            <div style={{ textAlign: "right", fontWeight: "bold", color: "#2e7d32", fontSize: 14 }}>
+                {product.price.toLocaleString()}원
+            </div>
+            {/* 별점 */}
+            <div style={{ marginTop: 4, color: "#fbc02d", fontSize: 14, textAlign: "left" }}>
+                {stars}
+            </div>
+        </div>
+    );
+}
