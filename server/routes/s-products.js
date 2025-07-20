c29bce0e8eef63ff581b97ecce3be8b7f72a236d
SungahChoo <blackdog03@naver.com>
2025-07-20 23:12:46 +0900

    feat: 상품등록 기본정보 입력~이미지 업로드

--- server/routes/s-products.js
+++ server/routes/s-products.js
@@ -4,6 +4,13 @@ const path = require('path');
 const router = express.Router();
 const { Product } = require('../models');
 
+// cloudinary
+const multer = require('multer');
+const { storage } = require('../modules/cloudinary');
+const upload = multer({ storage });
+
+const { ProductImg } = require('../models');
+
 // 상품 기본 정보 저장 (불필요한 필드 유출 방지 위해 아래 항목 씀)
 router.get('/basic', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/basic.html'));
@@ -54,6 +61,33 @@ router.post('/basic', async (req, res) => {
 // 이미지 등록
 router.get('/:productId/images', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/images.html'));
+});
+
+// 이미지 업로드 및 DB 저장
+router.post('/:productId/images', upload.array('images', 5), async (req, res) => {
+  const productId = req.params.productId;
+
+  try {
+    const imageUrls = req.files.map(file => file.path);
+    console.log('업로드된 이미지 URL:', imageUrls);
+
+    for (let i = 0; i < imageUrls.length; i++) {
+      await ProductImg.create({
+        product_id: productId,
+        img_url: imageUrls[i],
+        img_order: i + 1,
+        created_at: new Date(),
+      });
+    }
+
+    res.status(200).json({
+      message: '이미지 업로드 및 저장 완료',
+      urls: imageUrls
+    });
+  } catch (err) {
+    console.error('이미지 업로드 실패:', err);
+    res.status(500).json({ message: '업로드 실패', error: err.message });
+  }
 });
 
 module.exports = router;
