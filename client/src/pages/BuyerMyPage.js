51d20a91fa62a876b8525fbff62cdc25fd0f43a2
Encho <sunbi2003@gmail.com>
2025-07-30 14:56:52 +0900

    feat: 구매자 마이페이지 UI 완료

--- client/src/pages/BuyerMyPage.js
+++ client/src/pages/BuyerMyPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MyProfile from '../components/mypage/MyProfile';
 import OrderQueryBox from '../components/mypage/OrderQueryBox';
+import MyListSection from '../components/mypage/MyListSection';
+import InquirySection from '../components/mypage/InquirySection';
 
 export default function BuyerMyPage(){
     return(
         <div>
             <MyProfile />
-            <OrderQueryBox />
+            <div style={{marginBottom: '15px'}}><OrderQueryBox /></div>
+            <div style={{marginBottom: '15px'}}><MyListSection /></div>
+            <div style={{marginBottom: '15px'}}><InquirySection /></div>
         </div>
     );
 }
