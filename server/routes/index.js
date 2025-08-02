ce40739105f70eb320c9c9d02c309a373eb9074b
HaewonLee <ttoodong@naver.com>
2025-08-02 22:37:28 +0900

    feat: 추천 상품 목록 조회 API 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,14 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const authRoutes = require('./authRoutes'); 
-const mypageRoutes = require('./mypage-buyer') // 구매자 마이페이지
+const mypageRoutes = require('./mypage-buyer') 
+const recommendRoutes = require('./recommendRoutes');
 
 router.use('/auth', authRoutes);
 
 router.use('/my', mypageRoutes);
+
+router.use('/', recommendRoutes); //
 
 router.get('/', (req, res) => {
     res.send('Hello, Express');
--- server/routes/mypage-buyer/refundRoutes.js
+++ server/routes/mypage-buyer/refundRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../../middlewares/authMiddleware');
+const { authMiddleware, optionalAuth } = require('../../middlewares/authMiddleware');
 const { getMyCancellations, getCancellationDetail } = require('../../controllers/mypage-buyer/refundController');
 
 // 취소/반품 내역 조회
