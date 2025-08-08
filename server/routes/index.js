669a3ee5545451d9eb2b03c13bdb7c7b69a4992b
SungahChoo <blackdog03@naver.com>
2025-08-08 20:11:35 +0900

    feat: 판매자상품등록_기본 정보 등록

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const mypageRoutes = require('./mypage-buyer') // 구매자 마이페이지
 const sellerRoutes = require('./seller'); // 판매자 정보 조회 및 변경
 const storeInfoRoutes = require('./storeInfo'); // 스토어 정보 조회 및 변경
 const storeRoutes = require('./store'); // 스토어와 스토어 내 상품 관리
+const sProductsRoutes = require('./sProducts'); // 판매자 상품 등록
 
 router.use('/auth', authRoutes);
 
@@ -17,6 +18,8 @@ router.use('/seller', sellerRoutes);
 router.use('/store', storeInfoRoutes);
 
 router.use('/store', storeRoutes);
+
+router.use('/s-products', sProductsRoutes);
 
 router.get('/', (req, res) => {
     res.send('Hello, Express');
---
+++ server/routes/sProducts.js
@@ -0,0 +1,29 @@
+// server/routes/s-products.js
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+const controller = require('../controllers/sProduct/basic');
+// const upload = require('../middlewares/upload');
+
+
+// 상품 기본 정보
+router.post('/basic', authMiddleware,controller.postBasicInfo);
+
+// 이미지 등록
+// router.get('/:productId/images', controller.getImagesPage);
+// router.post('/:productId/images', upload.array('images', 5), controller.uploadImages);
+
+// // AI 상세 설명 생성 및 저장
+// router.get('/:productId/description/ai-gen', controller.getAIGenPage);
+// router.post('/:productId/description/ai-gen', controller.generateAIDescription);
+// router.post('/:productId/description/ai-save', controller.saveAIDescription);
+
+// // 상세 설명 직접 작성 및 저장
+// router.get('/:productId/description/manual', controller.getManualPage);
+// router.post('/:productId/description/manual', controller.saveManualDescription);
+
+// // AI 상세 설명 요약
+// router.get('/:productId/description/summary', controller.getSummaryDescription);
+// router.post('/:productId/description/summary-save', controller.saveSummaryToIntro);
+
+module.exports = router;
